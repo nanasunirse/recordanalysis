8cb49711be574bdc9bbc3bc4712f411780daa06c
nanasunirse <onishi.nanami.sunrise.hs@gmail.com>
2026-08-01 14:11:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,14 @@
 import streamlit as st
 uploaded_file = st.file_uploader("upload your food log")
 
+if uploaded_file is not None:
+wb = load_workbook(
+uploaded_file,
+data_only=True,
+keep_vba=True
+)
+st.success("File loaded successfully")
+
 import pandas as pd
 if uploaded_file is not None:
     df = pd.read_csv(
