d41797e45ac535094e76cfe269ce30862f7a8d10
nanasunirse <onishi.nanami.sunrise.hs@gmail.com>
2026-08-01 14:32:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,5 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file is not None:
-
-    temp_path = Path("uploaded_report.xlsm")
-
-    with open(temp_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-
-    st.write(temp_path)
-
-    report = parse_workbook(temp_path)
+df = pd.read_excel(uploaded_file)
+st.write(df.head())
