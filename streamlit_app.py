b4ee7fb5f848077560d7930ccdcf498ad8dc28ca
nanasunirse <onishi.nanami.sunrise.hs@gmail.com>
2026-08-01 13:12:08 +0700

    Set page title and icon for Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+st.set_page_config(page_title="メインページ", page_icon='icon.png')
+st.title("Multiple OSS Access Log Analyzer")
 
 import streamlit as st
 uploaded_file = st.file_uploader("upload your food log")
