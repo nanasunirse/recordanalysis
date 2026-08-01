23336716f313efeec32d388c76156c017da59c3c
nanasunirse <onishi.nanami.sunrise.hs@gmail.com>
2026-08-01 14:25:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 # st.set_page_config(page_title="メインページ", page_icon='icon.png') #show icon
 # st.title("Multiple OSS Access Log Analyzer")
 
-import streamlit as st
-uploaded_file = st.file_uploader("upload your food log")
 
-from openpyxl import load_workbook
+import streamlit as st
+from pathlib import Path
 
 uploaded_file = st.file_uploader(
     "Upload your food log",
@@ -13,12 +12,11 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
 
-    wb = load_workbook(
-        uploaded_file,
-        data_only=True,
-        keep_vba=True
-    )
+    temp_path = Path("uploaded_report.xlsm")
 
-    st.success("File loaded successfully")
+    with open(temp_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
 
+    st.write(temp_path)
 
+    report = parse_workbook(temp_path)
